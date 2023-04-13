1da2357cbe608469ea183255492cf4fa16f4d325
consr2 <qqq4861@naver.com>
2023-04-13 21:46:12 +0900

    코딩애플 끝

--- boardVue/src/App.vue
+++ boardVue/src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  
+  <div class="black-bg" v-if="modal == true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세 페이지 내용</p>
+      <p @click="modal = false">닫기</p>
+    </div>
+
+  </div>
+
+
   <div class="nav">
     <a v-for="i in navList" :key="i" href="#">{{ i }}</a>
   </div>
   
   <div class="oneroom">
-    <div v-for="(oneroom,i) in oneroom" :key="oneroom">
-      <p>{{ oneroom.name }}</p>
+    <div v-for="(oneroom,i) in onerooms" :key="i">
+      <img :src="oneroom.image" alt="roomIMG">
+      <h4 @click="modal = true">{{ oneroom.title }}</h4>
+      <p>{{ oneroom.content }}</p>
       <p>{{ oneroom.price }}만원</p>
-      <button @click="addclick(i)">추천</button>
-      <span>{{ oneroom.recomend }}</span>
     </div>
   </div>
 
@@ -17,30 +27,15 @@
 </template>
 
 <script>
+import data from './assets/oneroom';
 
 export default {
   name: 'App',
   data(){
     return{
       navList : ['Home', 'about', 'color'],
-      oneroom : [
-        {
-          name : '소원원룸',
-          price : 60,
-          recomend : 0
-        },
-        {
-          name : '미소원룸',
-          price : 30,
-          recomend : 0
-        },
-        {
-          name : '창원원룸',
-          price : 70,
-          recomend : 0
-        },
-      ],
-      
+      modal : false,
+      onerooms : data,
     }
   },
   methods:{
@@ -76,5 +71,16 @@ export default {
   border: 1px;
   border-color: black;
   border-radius: 1px;
+}
+
+.black-bg{
+  width: 100%; height: 100%;
+  background: rgb(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg{
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
